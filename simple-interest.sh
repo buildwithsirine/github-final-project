23a2ee4cc90a97218b5a888969d1272f95b76b99
buildwithsirine <sirine.amari002@gmail.com>
2025-06-25 19:28:01 +0200

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Ce script calcule l'intérêt simple donné le principal,
+# le taux d'intérêt annuel et la période de temps en années.
+# Ne pas utiliser ceci en production. À des fins d'exemple uniquement.
+# Auteur : Upkar Lidder (IBM)
+# Auteurs supplémentaires :
+# <ton nom d’utilisateur GitHub>
+
+echo "Entrez le principal :"
+read p
+echo "Entrez le taux d'intérêt par an :"
+read r
+echo "Entrez la période de temps en années :"
+read t
+s=`expr $p \* $t \* $r / 100`
+echo "L'intérêt simple est : "
+echo $s
